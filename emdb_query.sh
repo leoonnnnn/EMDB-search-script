76a043994dd14718e5c06578917588268a9f0f3c
leoonnnnn <39069236+leoonnnnn@users.noreply.github.com>
2023-10-26 12:46:38 -0700

    the first version
    not actually essential, just to see the progression

--- emdb_query.sh
+++ emdb_query.sh
@@ -13,17 +13,10 @@ echo "$@"
 
 # usage: script_name search_string
 
-# 1st draft, keep just to save to github, delete later
-# curl -X 'GET' \
-#   "https://www.ebi.ac.uk/emdb/api/search/$@" \
-#   -H 'accept: text/csv' \
-#   -H 'X-CSRFToken: 1ziFM3XS75jURyzFvTTdktH09yIED0uRVoFR4zmCRPU0CDIWtSSCoporzbGQv4lJ' \          # can prob delete these bottom 3
-#   -H 'Cache-Control: no-cache' \
-#   -H 'Pragma: no-cache'
-
 curl -X 'GET' \
-  "https://www.ebi.ac.uk/emdb/api/search/$@?fl=emdb_id%2Ctitle%2Cresolution%2Cfitted_pdbs%2Cxref_UNIPROTKB%2Cxref_ALPHAFOLD" \
+  "https://www.ebi.ac.uk/emdb/api/search/$@" \
   -H 'accept: text/csv' \
-  -H 'X-CSRFToken: fXJIBIxS0as6wbgs1qpsLws2gctH0UXF9M6UTeWCKU3chgpJZpoRPs9tGPrTSYOx' \
+  -H 'X-CSRFToken: 1ziFM3XS75jURyzFvTTdktH09yIED0uRVoFR4zmCRPU0CDIWtSSCoporzbGQv4lJ' \          # can prob delete these bottom 3
   -H 'Cache-Control: no-cache' \
   -H 'Pragma: no-cache'
+
